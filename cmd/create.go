65678a377c7bac76fa697009410a93c9a5d22bd1
Brad Cypert <brad.cypert@gmail.com>
2019-05-19 12:15:14 -0400

    path based creation for migrations

--- cmd/create.go
+++ cmd/create.go
@@ -16,11 +16,32 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
-// createCmd represents the create command
+var outputDir string
+
+func createFile(path string) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = f.Close()
+
+	return err
+}
+
+func makeTimestamp() string {
+	return strconv.FormatInt(time.Now().UnixNano() / int64(time.Millisecond), 10)
+}
+
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Creates a new migration",
@@ -28,14 +49,32 @@ var createCmd = &cobra.Command{
 any migration with this command, however.
 
 Use: deckard create add_login_date_to_users`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
+
+		// Add in the / suffix if it wasn't added by the user
+		if len(outputDir) > 0 && !strings.HasSuffix(outputDir, "/") {
+			outputDir += "/"
+		}
+
+		filepath := outputDir + makeTimestamp() + "__" + args[0]
+		upError := createFile(filepath + ".up.sql")
+		downError := createFile(filepath + ".down.sql")
+
+		if upError != nil {
+			log.Fatal(upError)
+		}
+
+		if downError != nil {
+			log.Fatal(downError)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-
+	createCmd.Flags().StringVarP(&outputDir, "outputDir", "o", "", "Output directory to write the migration to, defaults to current directory.")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
