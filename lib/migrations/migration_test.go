b3a8caecf247ebdfb89d4efca2f5d653dab37dfc
Brad Cypert <brad.cypert@gmail.com>
2019-07-26 22:39:31 -0700

    small test for create

--- lib/migrations/migration_test.go
+++ lib/migrations/migration_test.go
@@ -1,6 +1,9 @@
 package migrations
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -10,4 +13,25 @@ func TestMakeTimestamp(t *testing.T) {
 	if timestamp != "955946684000" {
 		t.Error("Expected to find:", 0, "instead got:", timestamp)
 	}
+}
+
+func TestCreate(t *testing.T) {
+	os.Mkdir("./tmp", 0777)
+	Create("./tmp", "test_create")
+
+	files, err := ioutil.ReadDir("./tmp")
+
+	if err != nil {
+		t.Error("Unable to create file")
+	}
+
+	if !strings.Contains(files[0].Name(), "test_create") {
+		t.Error("Unable to find created file. Creation may have failed.")
+	}
+
+	if len(files) < 2 {
+		t.Error("Did not create an up and a down migration.")
+	}
+
+	os.RemoveAll("./tmp")
 }
